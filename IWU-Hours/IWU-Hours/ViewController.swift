9a3ea2cb3f5318977337fc13525ea92be9332381
micahmaness <manessmic@gmail.com>
2018-02-08 20:08:21 -0500

    Xcode Files

---
+++ IWU-Hours/IWU-Hours/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IWU-Hours
+//
+//  Created by Micah Maness on 2/8/18.
+//  Copyright © 2018 Micah Maness. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
