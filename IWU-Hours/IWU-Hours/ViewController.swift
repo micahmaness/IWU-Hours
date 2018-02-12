e2a9e1e85162f6240e59404c4ca081add0e76980
micahmaness <manessmic@gmail.com>
2018-02-12 13:45:29 -0500

    More xcode

--- IWU-Hours/IWU-Hours/ViewController.swift
+++ IWU-Hours/IWU-Hours/ViewController.swift
@@ -5,18 +5,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
+        for i in 0...1000 {
+            data.append("\(i)")
+        }
+        
+
     }
+    
+    private var data: [String] = []
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")! //1.
+        
+        let text = data[indexPath.row] //2.
+        
+        cell.textLabel?.text = text //3.
+        
+        return cell //4.
+    }
 
 }
