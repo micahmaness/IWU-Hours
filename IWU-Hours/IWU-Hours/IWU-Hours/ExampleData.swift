71358f55b8b61eb5985cf71dbb985d9adfd22869
samdief <sam.diefenbacher@myemail.indwes.edu>
2018-02-15 19:50:30 -0500

    Add files via upload
    Finished Project

---
+++ IWU-Hours/IWU-Hours/IWU-Hours/ExampleData.swift
@@ -0,0 +1,45 @@
+// IWU Hours
+// Micah Maness, Matt Babb, Sam D
+//
+
+import Foundation
+
+public struct Item {
+    var name: String
+    var detail: String
+    
+    public init(name: String, detail: String) {
+        self.name = name
+        self.detail = detail
+    }
+}
+
+public struct Section {
+    var name: String
+    var items: [Item]
+    var collapsed: Bool
+    
+    public init(name: String, items: [Item], collapsed: Bool = false) {
+        self.name = name
+        self.items = items
+        self.collapsed = collapsed
+    }
+}
+
+public var sectionsData: [Section] = [
+    Section(name: "IWU Hours", items: [
+        Item(name: "Baldwin", detail: "BREAKFAST\nMonday - Friday: 7:00 a.m - 10:00 a.m * No Breakfast on Weekends\n\nLUNCH\nMonday - Friday: 11:00 a.m. - 2:00 p.m.\nSaturday: 11:30 a.m - 12:30 p.m.\nSunday: 11:30 a.m - 1:15 p.m.\n\nDINNER\nMonday-Thursday 5:00 p.m. - 6:30 p.m.\nFriday-Sunday: 5:00 p.m. - 6:00 p.m."),
+        Item(name: "Wildcat Express", detail: "Monday - Friday: 8:30 a.m. - 9:00 p.m. \nSaturday: 12:30 p.m - 5:00 p.m."),
+        Item(name: "Trader James", detail: "Monday - Friday: 10:00 a.m. - Midnight\nSaturday: 2:00 p.m - Midnight"),
+        Item(name: "Marios Pizza / Wilbur's Wild Wings", detail: "Monday - Friday: 10:00 a.m. - Midnight\nSaturday-Sunday: 6:00 p.m - 12:00 a.m. Midnight "),
+        Item(name: "McConn", detail: "Monday - Friday: 7:00 a.m. - 11:30 p.m\nSaturday: 9:00 a.m - 11:30 p.m\nSunday: 7:00 PM - 11:30 p.m\n*MWF closed from 9:30 - 11:00 for chapel services."),
+        Item(name: "Jackson Library", detail: "Monday - Thursday: 7:30 a.m. - Midnight\nFriday: 7:30 a.m. to 6 p.m.\nSaturday: 11 a.m. - 8 p.m."),
+        Item(name: "Recreation and Wellness Center", detail: "Monday - Thursday: 6 a.m. - 11 p.m. \nFriday: 6 a.m. - 7 p.m. \nSaturday: 8 a.m. - 7 p.m. \nSunday: 2 p.m. - 5 p.m., 8 p.m. - 10 p.m.\n*Free Weight Room closes M-F from 8 a.m. - 12:00 p.m."),
+        Item(name: "Rock Wall", detail: "Monday - Thursday: 6 p.m. - 10 p.m.\nFriday: 4 p.m. - 7 p.m.\nSaturday: 1 p.m. - 5 p.m.\nSunday: CLOSED"),
+    ]),
+
+]
+
+
+
+
